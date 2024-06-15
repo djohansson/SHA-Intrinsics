3d4edf8f1eee4bafa129e383793cf127710821f8
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-06-16 00:36:33 +0200

    forgot x86 includes

--- sha-intrinsics.h
+++ sha-intrinsics.h
@@ -8,6 +8,9 @@
 #define SHA256_PROCESS sha256_process_arm
 #define SHA512_PROCESS sha512_process
 #elif defined(__i386__) || defined(_M_IX86) || defined(_X86_) || defined(__x86_64__) || defined(_M_X64)
+#include "sha1-x86.h"
+#include "sha256-x86.h"
+#include "sha512.h"
 #define SHA1_PROCESS sha1_process_x86
 #define SHA256_PROCESS sha256_process_x86
 #define SHA512_PROCESS sha512_process
