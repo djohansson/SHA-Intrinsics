9dd886303c2a4731345a93e24b7f3d6b6efed007
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-06-16 00:10:32 +0200

    add sha-intrinsics.h

---
+++ sha-intrinsics.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#if defined(__arm__) || defined(__aarch32__) || defined(__arm64__) || defined(__aarch64__) || defined(_M_ARM)
+#include "sha1-arm.h"
+#include "sha256-arm.h"
+#include "sha512.h"
+#define SHA1_PROCESS sha1_process_arm
+#define SHA256_PROCESS sha256_process_arm
+#define SHA512_PROCESS sha512_process
+#elif defined(__i386__) || defined(_M_IX86) || defined(_X86_) || defined(__x86_64__) || defined(_M_X64)
+#define SHA1_PROCESS sha1_process_x86
+#define SHA256_PROCESS sha256_process_x86
+#define SHA512_PROCESS sha512_process
+#else
+#define SHA256_PROCESS sha256_process
+#define SHA512_PROCESS sha512_process
+#endif
